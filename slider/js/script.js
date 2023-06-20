de6ede47a13c43d75b3328d6848f481c29ad4466
Sumaiya Aktar <sumaiyaaktar3029@gmail.com>
2023-06-20 23:02:28 +0600

    add

--- slider/js/script.js
+++ slider/js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+  // lightbox
+  lightbox.option({
+    resizeDuration: 200,
+    wrapAround: true,
+  });
+
   // slick
   $(".wrapper").slick({
     infinite: true,
